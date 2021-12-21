25efaadc34860e1e1b0795f9e9ef8818b8a55796
Biswajee <roy.biswajeet161@gmail.com>
2021-12-22 00:27:36 +0530

    test test test

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,3 @@
 ./opa run --server --addr :$PORT &
-curl -X PUT -d @policy/sshd_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sshd/authz --header "Content-Type: text/plain"
+curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz --header "Content-Type: text/plain"
 curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d @data/admin-users.json --header "Content-Type: application/json"
