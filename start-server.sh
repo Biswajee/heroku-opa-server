21b9d50bb332b867b5b1b2feb890447d535d27ad
Biswajee <roy.biswajeet161@gmail.com>
2021-12-22 00:42:18 +0530

    continue waiting for the server

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 ./opa run --server --addr :$PORT &
 curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz --header "Content-Type: text/plain"
 curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d @data/admin-users.json --header "Content-Type: application/json"
-fg
+wait
