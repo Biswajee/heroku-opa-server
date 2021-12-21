383a76c329fa2dfaa649e35095e7254b2e94a98f
Biswajee <roy.biswajeet161@gmail.com>
2021-12-22 00:17:50 +0530

    Add debug statements

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,5 @@
+set -e -x
+ls -al
 ./opa run --server --addr :$PORT &
 curl -X PUT --data-binary @policy/sshd_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sshd/authz
 curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d -d @data/admin-users.json --header "Content-Type: application/json"
