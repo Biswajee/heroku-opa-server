d14d9e69143640a238106badfb15eee1182b676a
Biswajee <roy.biswajeet161@gmail.com>
2021-12-22 00:20:13 +0530

    Remove invalid character

--- start-server.sh
+++ start-server.sh
@@ -2,4 +2,4 @@ set -e -x
 ls -al
 ./opa run --server --addr :$PORT &
 curl -X PUT --data-binary @policy/sshd_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sshd/authz
-curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d -d @data/admin-users.json --header "Content-Type: application/json"
+curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d @data/admin-users.json --header "Content-Type: application/json"
