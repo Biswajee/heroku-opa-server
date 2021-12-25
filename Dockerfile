f2a23802557cc590a3cf456099164c75a317d020
Biswajee <roy.biswajeet161@gmail.com>
2021-12-25 19:54:40 +0530

    refinement

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,7 @@ RUN apt-get update && apt-get install -y curl
 RUN curl -L -o opa https://openpolicyagent.org/downloads/v0.35.0/opa_linux_amd64_static
 RUN chmod 755 ./opa
 COPY policy policy
-COPY data data
 COPY start-server.sh .
 RUN chmod 777 start-server.sh
-RUN chmod -R 777 policy data
+RUN chmod -R 777 policy
 CMD ./start-server.sh
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,4 @@
 ./opa run --server --addr :$PORT &
 curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz --header "Content-Type: text/plain"
-# curl -X PUT -d @data/admin-users.json opa-auth-server.herokuapp.com:80/v1/data/roles --header "Content-Type: application/json"
 curl -X GET opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz
-# curl -X GET opa-auth-server.herokuapp.com:80/v1/data/roles
 wait
