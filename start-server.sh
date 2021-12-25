cdf42dffbf261b1cf14206cdb67c66c9677cb7b8
Biswajit Roy <roy.biswajeet161@gmail.com>
2021-12-25 14:08:23 +0530

    Update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 ./opa run --server --addr :$PORT &
-curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz --header "Content-Type: text/plain"
-curl -X PUT -d @data/admin-users.json opa-auth-server.herokuapp.com:80/v1/data/sudo/authz/roles --header "Content-Type: application/json"
+curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz/sudo_authz --header "Content-Type: text/plain"
+curl -X PUT -d @data/admin-users.json opa-auth-server.herokuapp.com:80/v1/data/roles --header "Content-Type: application/json"
 curl -X GET opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz
-curl -X GET opa-auth-server.herokuapp.com:80/v1/data/sudo/authz/roles
+curl -X GET opa-auth-server.herokuapp.com:80/v1/data/roles
 wait
