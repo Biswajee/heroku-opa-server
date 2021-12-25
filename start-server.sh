cedf66b4d3faa37067c402bbbf81a05b00384af8
Biswajit Roy <roy.biswajeet161@gmail.com>
2021-12-25 11:42:09 +0530

    Add statements to confirm changes made to server

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,6 @@
 ./opa run --server --addr :$PORT &
 curl -X PUT -d @policy/sudo_authz.rego opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz --header "Content-Type: text/plain"
-curl -X PUT opa-auth-server.herokuapp.com:80/v1/data/roles -d @data/admin-users.json --header "Content-Type: application/json"
+curl -X PUT -d @data/admin-users.json opa-auth-server.herokuapp.com:80/v1/data/roles --header "Content-Type: application/json"
+curl -X GET opa-auth-server.herokuapp.com:80/v1/policies/sudo/authz
+curl -X GET opa-auth-server.herokuapp.com:80/v1/data/roles
 wait
